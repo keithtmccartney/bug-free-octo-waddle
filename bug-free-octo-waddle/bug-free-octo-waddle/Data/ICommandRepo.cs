09fa2557cb66bb1849991fd2d119297ed5498e5c
Keith <keithtmccartney@hotmail.com>
2020-10-19 11:01:35 +0100

    Applying Interface changes and creating a new Mock repository.

--- bug-free-octo-waddle/bug-free-octo-waddle/Data/ICommandRepo.cs
+++ bug-free-octo-waddle/bug-free-octo-waddle/Data/ICommandRepo.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using bug_free_octo_waddle.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace bug_free_octo_waddle.Data
 {
-	interface ICommandRepo
+	public interface ICommandRepo
 	{
+		IEnumerable<Command> GetAppCommands();
+		Command GetCommandById(int id);
 	}
 }
---
+++ bug-free-octo-waddle/bug-free-octo-waddle/Data/MockCommandRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bug_free_octo_waddle.Data
+{
+	public class MockCommandRepo
+	{
+	}
+}
