e1ca3575e4c6e2427b05423e112bf602a79c2893
Keith <keithtmccartney@hotmail.com>
2020-10-19 12:53:57 +0100

    Creating a new 'Commands' Controller.

---
+++ bug-free-octo-waddle/Commander/Controllers/CommandsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommandRepo _mockCommandRepo = new MockCommandRepo();
+
+        //GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _mockCommandRepo.GetAppCommands();
+
+            return Ok(commandItems);
+        }
+
+        //GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _mockCommandRepo.GetCommandById(id);
+
+            return Ok(commandItem);
+        }
+    }
+}
