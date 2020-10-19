890c6ee55f8dbbea2b0d77d36d2ea640bac6a94d
Keith <keithtmccartney@hotmail.com>
2020-10-19 12:46:44 +0100

    Changing the Startup logic, and creating a new 'Commander' API project.

--- bug-free-octo-waddle/Commander/Startup.cs
+++ bug-free-octo-waddle/Commander/Startup.cs
@@ -1,12 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace bug_free_octo_waddle
+namespace Commander
 {
 	public class Startup
 	{
@@ -20,15 +25,7 @@ namespace bug_free_octo_waddle
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			//services.AddControllersWithViews();
-
 			services.AddControllers();
-
-			// In production, the Angular files will be served from this directory
-			services.AddSpaStaticFiles(configuration =>
-			{
-				configuration.RootPath = "ClientApp/dist";
-			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,12 +34,6 @@ namespace bug_free_octo_waddle
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
 			}
 
 			app.UseHttpsRedirection();
@@ -54,25 +45,6 @@ namespace bug_free_octo_waddle
 			app.UseEndpoints(endpoints =>
 			{
 				endpoints.MapControllers();
-			});
-
-			app.UseStaticFiles();
-			if (!env.IsDevelopment())
-			{
-				app.UseSpaStaticFiles();
-			}
-
-			app.UseSpa(spa =>
-			{
-				// To learn more about options for serving an Angular SPA from ASP.NET Core,
-				// see https://go.microsoft.com/fwlink/?linkid=864501
-
-				spa.Options.SourcePath = "ClientApp";
-
-				if (env.IsDevelopment())
-				{
-					spa.UseAngularCliServer(npmScript: "start");
-				}
 			});
 		}
 	}
