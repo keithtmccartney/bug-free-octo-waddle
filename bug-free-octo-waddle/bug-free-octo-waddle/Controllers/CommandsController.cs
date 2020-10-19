6ebafc1fdf5287d367660fc5b7dc250da1c6796a
Keith <keithtmccartney@hotmail.com>
2020-10-19 11:31:30 +0100

    Applying initial changes to the 'Commands' Controller.

--- bug-free-octo-waddle/bug-free-octo-waddle/Controllers/CommandsController.cs
+++ bug-free-octo-waddle/bug-free-octo-waddle/Controllers/CommandsController.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using bug_free_octo_waddle.Data;
+using bug_free_octo_waddle.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace bug_free_octo_waddle.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/commands")]
     [ApiController]
     public class CommandsController : ControllerBase
     {
+        private readonly MockCommandRepo _mockCommandRepo = new MockCommandRepo();
+
+        //GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _mockCommandRepo.GetAppCommands();
+
+            return Ok(commandItems);
+        }
+
+        //GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _mockCommandRepo.GetCommandById(id);
+
+            return Ok(commandItem);
+        }
     }
 }
