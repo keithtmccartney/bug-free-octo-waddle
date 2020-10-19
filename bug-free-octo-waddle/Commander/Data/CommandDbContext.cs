a17d8816bce6d8869610aa2a1444962e263535bd
Keith <keithtmccartney@hotmail.com>
2020-10-19 14:43:54 +0100

    Adding a base database context.

---
+++ bug-free-octo-waddle/Commander/Data/CommandDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Commander.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+	public class CommandDbContext : DbContext
+	{
+		public CommandDbContext(DbContextOptions<CommandDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Command> Commands { get; set; }
+	}
+}
