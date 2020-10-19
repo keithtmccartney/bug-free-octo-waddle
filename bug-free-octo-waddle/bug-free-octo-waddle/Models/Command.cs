56b849798d05a57c3150c9a195f924b655522ef7
Keith <keithtmccartney@hotmail.com>
2020-10-19 10:57:07 +0100

    Adding a 'Command' Interface.

--- bug-free-octo-waddle/bug-free-octo-waddle/Models/Command.cs
+++ bug-free-octo-waddle/bug-free-octo-waddle/Models/Command.cs
@@ -7,5 +7,9 @@ namespace bug_free_octo_waddle.Models
 {
 	public class Command
 	{
+		public int Id { get; set; }
+		public string HowTo { get; set; }
+		public string Line { get; set; }
+		public string Platform { get; set; }
 	}
 }
