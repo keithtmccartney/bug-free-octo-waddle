0941390c594fda7f931550f6c553c9ce2fefb6d9
Keith <keithtmccartney@hotmail.com>
2020-10-20 10:35:42 +0100

    Creating a 'Command' SQL repository.

---
+++ bug-free-octo-waddle/Commander/Data/SqlCommanderRepo.cs
@@ -0,0 +1,28 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+	public class SqlCommanderRepo : ICommanderRepo
+	{
+		private readonly CommanderContext _context;
+
+		public SqlCommanderRepo(CommanderContext context)
+		{
+			_context = context;
+		}
+
+		public IEnumerable<Command> GetAllCommands()
+		{
+			return _context.Commands.ToList();
+		}
+
+		public Command GetCommandById(int id)
+		{
+			return _context.Commands.FirstOrDefault(p => p.Id == id);
+		}
+	}
+}
