1fc68a666ffa8f20f87ea85b15374d027c4388a7
Keith <keithtmccartney@hotmail.com>
2020-10-20 10:10:51 +0100

    Fixing some typos as per the tutorial.

--- bug-free-octo-waddle/Commander/Data/ICommanderRepo.cs
+++ bug-free-octo-waddle/Commander/Data/ICommanderRepo.cs
@@ -8,7 +8,7 @@ namespace Commander.Data
 {
 	public interface ICommanderRepo
 	{
-		IEnumerable<Command> GetAppCommands();
+		IEnumerable<Command> GetAllCommands();
 		Command GetCommandById(int id);
 	}
 }
