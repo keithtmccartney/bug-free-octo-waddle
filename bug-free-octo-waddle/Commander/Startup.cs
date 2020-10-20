cce00ec4ab2952fe0c21672ecdc44dc43fd7d826
Keith <keithtmccartney@hotmail.com>
2020-10-20 10:37:37 +0100

    Changing our Startup scope configuration.

--- bug-free-octo-waddle/Commander/Startup.cs
+++ bug-free-octo-waddle/Commander/Startup.cs
@@ -31,7 +31,7 @@ namespace Commander
 
 			services.AddControllers();
 
-			services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+			services.AddScoped<ICommanderRepo, SqlCommanderRepo>(); // Use MockCommanderRepo for a mock repository;
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
