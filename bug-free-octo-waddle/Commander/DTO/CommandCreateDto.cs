ef83af812d728e8e8f3efc899231b851b73cfd73
Keith <keithtmccartney@hotmail.com>
2020-10-20 13:13:47 +0100

    Applying changes to the 'Command' Create DTO to encapsulate any 500 HTTP codes being returned.

--- bug-free-octo-waddle/Commander/DTO/CommandCreateDto.cs
+++ bug-free-octo-waddle/Commander/DTO/CommandCreateDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,12 @@ namespace Commander.DTO
 {
 	public class CommandCreateDto
 	{
+		[Required]
+		[MaxLength(250)]
 		public string HowTo { get; set; }
+		[Required]
 		public string Line { get; set; }
+		[Required]
 		public string Platform { get; set; }
 	}
 }
